a9394dbf105ce12bc78ba8127b83b957514601c9
jjelosua <jelosua@npr.org>
2016-11-14 10:00:44 -0500

    add quick access to document

--- app_config.py
+++ app_config.py
@@ -258,7 +258,7 @@ def configure_targets(deployment_target):
         DEBUG = True
         ASSETS_MAX_AGE = 20
         # STAGING DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '1lov0ktOxZFre9x-WrfKK9roan5jAs9XP6vhPY9lLNo0'
+        TRANSCRIPT_GDOC_KEY = '1b1NrTa7OUXEqZ9wkL2Ps_Hal13tC39XoVkpgvb65bw4'
         # STAGING LOGS
         GAS_LOG_KEY = '1vpRgWpqGqW1p3yMv6nCixAjczc8cJr_TlMCTg52Ch9I'
     else:
