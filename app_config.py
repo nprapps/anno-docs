f3cf747f64f1c2a381886f13b579fc1a68b7285a
jjelosua <jelosua@npr.org>
2017-01-13 20:39:42 +0100

    add SEAMUS id for inauguration factcheck

--- app_config.py
+++ app_config.py
@@ -40,7 +40,7 @@ ASSETS_SLUG = 'debates'
 # DEPLOY SETUP CONFIG
 DEBATE_DIRECTORY_PREFIX = 'factchecks/'
 CURRENT_DEBATE = '20170111-trump-press-conference-factcheck'
-SEAMUS_ID = '509137239'  # SEAMUS PAGE ID FOR DEEP LINKING
+SEAMUS_ID = '509703302'  # SEAMUS PAGE ID FOR DEEP LINKING
 try:
     from local_settings import CURRENT_DEBATE
     # Override SEAMUS_ID to generate the sharing list accordingly
