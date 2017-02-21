8342ee7c0a3025b8779ae8171881aac5f890a107
jjelosua <jelosua@npr.org>
2017-02-21 15:37:20 +0100

    chown recursive for node modules

--- fabfile/servers.py
+++ fabfile/servers.py
@@ -93,7 +93,7 @@ def install_requirements():
 
     run('%(SERVER_VIRTUALENV_PATH)s/bin/pip install -U -r %(SERVER_REPOSITORY_PATH)s/requirements.txt' % app_config.__dict__)
     sudo('cd %(SERVER_REPOSITORY_PATH)s; npm install' % app_config.__dict__)
-    sudo('chown ubuntu:ubuntu %(SERVER_REPOSITORY_PATH)s/node_modules' % app_config.__dict__)
+    sudo('chown -R ubuntu:ubuntu %(SERVER_REPOSITORY_PATH)s/node_modules' % app_config.__dict__)
 
 @task
 def setup_logs():
