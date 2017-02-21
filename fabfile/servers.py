02c4d53f627d953d9a798c8413a31d71537033d6
jjelosua <jelosua@npr.org>
2017-02-21 12:02:53 +0100

    sudo better on fabric

--- fabfile/servers.py
+++ fabfile/servers.py
@@ -92,7 +92,7 @@ def install_requirements():
     require('settings', provided_by=['production', 'staging'])
 
     run('%(SERVER_VIRTUALENV_PATH)s/bin/pip install -U -r %(SERVER_REPOSITORY_PATH)s/requirements.txt' % app_config.__dict__)
-    run('cd %(SERVER_REPOSITORY_PATH)s; sudo npm install' % app_config.__dict__)
+    sudo('cd %(SERVER_REPOSITORY_PATH)s; npm install' % app_config.__dict__)
 
 @task
 def setup_logs():
