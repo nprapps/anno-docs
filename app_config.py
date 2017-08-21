0c2ae166567b8dafc303ec2fc78e562dda05f3ba
jjelosua <jelosua@npr.org>
2017-08-20 23:05:59 -0400

    new factcheck

--- app_config.py
+++ app_config.py
@@ -39,7 +39,7 @@ ASSETS_SLUG = 'anno-docs'
 
 # DEPLOY SETUP CONFIG
 FACTCHECKS_DIRECTORY_PREFIX = 'factchecks/'
-CURRENT_FACTCHECK = '20170504-trump-religious-freedom-eo'
+CURRENT_FACTCHECK = '20170821-trump-afghanistan'
 try:
     # Override CURRENT FACTCHECK
     from local_settings import CURRENT_FACTCHECK
@@ -141,7 +141,7 @@ PARENT_FOLDER_ID = '0B6C-jdxmvrJoM3JnZ1ZZUkhVQTg'
 GAS_LOG_KEY = '1tUxTFa2J5IKIlOMLop9IA9eaZ6uDDhgh6KwxeLdgQGU' # Google app script logs spreadsheet key
 TRANSCRIPT_GDOC_KEY = '1Byvot9oRRS9gvm2nTFuO4dLiyOrI02f-Xhy6pbMk34s' # Google app script google doc key
 SCRIPT_PROJECT_NAME = 'factcheck_scripts' # Google app scripts project name
-CSPAN = False
+CSPAN = True
 """
 SHARING
 """
@@ -250,7 +250,7 @@ def configure_targets(deployment_target):
         DEBUG = False
         ASSETS_MAX_AGE = 86400
         # PRODUCTION DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '18AhgQ9uo2krA7AVwZSj2M4mgdTtdeILr3QUfp4uHguU'
+        TRANSCRIPT_GDOC_KEY = '1y8Wmd8nlDmVt-2L792cPW7A7Iy7lbHVvCt_dvLHuMgE'
         # PRODUCTION TEST DOCUMENT
         # TRANSCRIPT_GDOC_KEY = '1sK3FQ1VGh2bZZHoI2ontZWy_32UchVyIOZ2X5vbWJIo'
         # PRODUCTION LOGS
@@ -269,7 +269,7 @@ def configure_targets(deployment_target):
         DEBUG = True
         ASSETS_MAX_AGE = 20
         # STAGING DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '18AhgQ9uo2krA7AVwZSj2M4mgdTtdeILr3QUfp4uHguU'
+        TRANSCRIPT_GDOC_KEY = '1IJot3xfpcIS-RmqeGrkkQ8xQtmvxlnhMFb6LmL2_OvE'
         # STAGING LOGS
         GAS_LOG_KEY = '1vpRgWpqGqW1p3yMv6nCixAjczc8cJr_TlMCTg52Ch9I'
     else:
