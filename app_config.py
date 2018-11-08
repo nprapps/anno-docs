bdbedfbcabbfe219b0c29138e2079f18ac67a6df
Thomas Wilburn <twilburn@npr.org>
2018-11-08 12:57:33 -0500

    Demo with Miles

--- app_config.py
+++ app_config.py
@@ -39,7 +39,7 @@ ASSETS_SLUG = 'anno-docs'
 
 # DEPLOY SETUP CONFIG
 FACTCHECKS_DIRECTORY_PREFIX = 'factchecks/'
-CURRENT_FACTCHECK = '20180806-trump-stump-speech'
+CURRENT_FACTCHECK = '20181208-testdeploy'
 try:
     # Override CURRENT FACTCHECK
     from local_settings import CURRENT_FACTCHECK
@@ -139,7 +139,7 @@ GOOGLE APPS SCRIPTS
 
 PARENT_FOLDER_ID = '0B6C-jdxmvrJoM3JnZ1ZZUkhVQTg'
 GAS_LOG_KEY = '1tUxTFa2J5IKIlOMLop9IA9eaZ6uDDhgh6KwxeLdgQGU' # Google app script logs spreadsheet key
-TRANSCRIPT_GDOC_KEY = '1PMYtSTPcGapGE_VQwHx3fDi5naVl7_1Cpz2KCmmTq5A' # Google app script google doc key
+TRANSCRIPT_GDOC_KEY = '1gibir_r26oZ0gi3ujlXHtzuzNusQZUUj0KCuhYCdJSk' # Google app script google doc key
 SCRIPT_PROJECT_NAME = 'factcheck_scripts' # Google app scripts project name
 CSPAN = False
 """
@@ -250,7 +250,7 @@ def configure_targets(deployment_target):
         DEBUG = False
         ASSETS_MAX_AGE = 86400
         # PRODUCTION DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '1PMYtSTPcGapGE_VQwHx3fDi5naVl7_1Cpz2KCmmTq5A'
+        TRANSCRIPT_GDOC_KEY = '1gibir_r26oZ0gi3ujlXHtzuzNusQZUUj0KCuhYCdJSk'
         # PRODUCTION TEST DOCUMENT
         # TRANSCRIPT_GDOC_KEY = '1sK3FQ1VGh2bZZHoI2ontZWy_32UchVyIOZ2X5vbWJIo'
         # PRODUCTION LOGS
@@ -269,7 +269,7 @@ def configure_targets(deployment_target):
         DEBUG = True
         ASSETS_MAX_AGE = 20
         # STAGING DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '1PMYtSTPcGapGE_VQwHx3fDi5naVl7_1Cpz2KCmmTq5A'
+        TRANSCRIPT_GDOC_KEY = '1gibir_r26oZ0gi3ujlXHtzuzNusQZUUj0KCuhYCdJSk'
         # STAGING LOGS
         GAS_LOG_KEY = '1vpRgWpqGqW1p3yMv6nCixAjczc8cJr_TlMCTg52Ch9I'
     else:
@@ -283,7 +283,7 @@ def configure_targets(deployment_target):
         DEBUG = True
         ASSETS_MAX_AGE = 20
         # DEVELOPMENT DOCUMENT
-        TRANSCRIPT_GDOC_KEY = '1PMYtSTPcGapGE_VQwHx3fDi5naVl7_1Cpz2KCmmTq5A'
+        TRANSCRIPT_GDOC_KEY = '1gibir_r26oZ0gi3ujlXHtzuzNusQZUUj0KCuhYCdJSk'
         # DEVELOPMENT LOGS
         GAS_LOG_KEY = '1I7IUCUJHIWLW3c_E-ukfqIp4QxuvUoHqbEQIlKQFC7w'
         # Override S3_BASE_URL to use another port locally for fab app
