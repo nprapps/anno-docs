70536f34bb3d3e447d9ac8f4785f07f405f0479e
Thomas Wilburn <twilburn@npr.org>
2019-01-02 09:57:59 -0600

    Update for test

--- app_config.py
+++ app_config.py
@@ -39,7 +39,7 @@ ASSETS_SLUG = 'anno-docs'
 
 # DEPLOY SETUP CONFIG
 FACTCHECKS_DIRECTORY_PREFIX = 'factchecks/'
-CURRENT_FACTCHECK = '20180313-skillshare-annotations'
+CURRENT_FACTCHECK = '20190102-sotutest'
 try:
     # Override CURRENT FACTCHECK
     from local_settings import CURRENT_FACTCHECK
@@ -141,7 +141,7 @@ GOOGLE APPS SCRIPTS
 
 PARENT_FOLDER_ID = '0B6C-jdxmvrJoM3JnZ1ZZUkhVQTg'
 GAS_LOG_KEY = '1tUxTFa2J5IKIlOMLop9IA9eaZ6uDDhgh6KwxeLdgQGU' # Google app script logs spreadsheet key
-TRANSCRIPT_GDOC_KEY = '1Byvot9oRRS9gvm2nTFuO4dLiyOrI02f-Xhy6pbMk34s' # Google app script google doc key
+TRANSCRIPT_GDOC_KEY = '1nFvMWKEyRijnQyAfduu8BIrPZJdWQO7bwJ8zdTgmRY4' # Google app script google doc key
 SCRIPT_PROJECT_NAME = 'factcheck_scripts' # Google app scripts project name
 CSPAN = True
 """
