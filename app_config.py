876a59a1807a7a95c5bda6fb8fa083280cadaabf
Thomas Wilburn <twilburn@npr.org>
2019-02-04 09:40:15 -0600

    .

--- app_config.py
+++ app_config.py
@@ -44,7 +44,7 @@ ASSETS_SLUG = 'anno-docs'
 
 # DEPLOY SETUP CONFIG
 FACTCHECKS_DIRECTORY_PREFIX = 'factchecks/'
-CURRENT_FACTCHECK = '20190131-training'
+CURRENT_FACTCHECK = '20190205-sotu-trump'
 try:
     # Override CURRENT FACTCHECK
     from local_settings import CURRENT_FACTCHECK
